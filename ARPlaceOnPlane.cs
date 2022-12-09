58af50c69db03d317841f183f5ae0c3cc6454331
노원희 <87654809+neoseurae12@users.noreply.github.com>
2022-12-09 22:19:00 +0900

    Update ARPlaceOnPlane.cs
    our First AR project - C# code

--- ARPlaceOnPlane.cs
+++ ARPlaceOnPlane.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
-// import
+// imports
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
